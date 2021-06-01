c7470c863e7bbdc1b568bd1c65884e57b9e80a05
Nut Pinyo <bomb.np@gmail.com>
2021-06-01 22:29:29 +0700

    current footage and count

--- index.js
+++ index.js
@@ -3,24 +3,38 @@ function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms))
 }
 
-async function Counter() {
-  var start = 0 //get from recent count
-  var end = 68 //get new count from storage
+async function Counter(newValue) {
+  var start = Number(cnt.innerHTML) //get from recent count
+  var end = newValue //get new count from storage
   if (start < end)
     for (var i = start; i <= end; i++) {
-      await sleep(15)
+      await sleep(7)
       cnt.innerHTML = i.toString()
     }
   else {
     for (var i = start; i >= end; i--) {
-      await sleep(15)
+      await sleep(7)
       cnt.innerHTML = i.toString()
     }
   }
 }
-Counter()
+Counter(50)
 
-const delay = 1000
-function updateData() {}
+const delay = 2000
+const clientID = '40a845fd-6f0d-4fdb-8ade-305b40b02ae8'
+const token = 'VPGoWcFX6Ai1zmDC92yxMKAcJdEvXfp5'
+async function updateData() {
+  document.getElementById(
+    'footage'
+  ).src = `https://storage.googleapis.com/crowd-density-monitor/footage.jpg?t=${Date.now()}`
+  const response = await fetch('https://api.netpie.io/v2/device/shadow/data', {
+    headers: {
+      Authorization: `Device ${clientID}:${token}`,
+      'Content-Type': 'application/json',
+    },
+  })
+  const data = await response.json()
+  await Counter(data.data.peopleCount)
+}
 
 setInterval(updateData, delay)
