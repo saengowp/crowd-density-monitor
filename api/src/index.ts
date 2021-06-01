41a7ace3805ed06acf193b6d91867bc8126ae42e
Nut Pinyo <bomb.np@gmail.com>
2021-06-02 00:34:25 +0700

    change from file to raw body

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,3 +1,4 @@
+import { raw as bodyParserRaw } from 'body-parser'
 import { createCanvas } from 'canvas'
 import express, { Request, Response } from 'express'
 import multer from 'multer'
@@ -14,38 +15,34 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Crowd Density Monitor API')
 })
 
-app.post(
-  '/footage',
-  upload.single('image'),
-  async (req: Request, res: Response) => {
-    const buffer = req.file.buffer
-    const start = new Date()
-    console.log(`${start} - Received footage `)
+app.post('/footage', bodyParserRaw(), async (req: Request, res: Response) => {
+  const buffer = req.body
+  const start = new Date()
+  console.log(`${start} - Received footage `)
 
-    const canvas = createCanvas(174, 144)
-    const ctx = canvas.getContext('2d')
+  const canvas = createCanvas(174, 144)
+  const ctx = canvas.getContext('2d')
 
-    const imageData = await constructImageData(buffer)
-    ctx.putImageData(imageData, 0, 0)
+  const imageData = await constructImageData(buffer)
+  ctx.putImageData(imageData, 0, 0)
 
-    const people = (await objectLocalize(canvas.toBuffer())).filter(
-      (object) => object.name === 'Person'
-    )
-    for (const person of people) {
-      drawBox(canvas, person.boundingPoly.normalizedVertices)
-    }
-
-    updatePeopleCount(people.length)
-    await uploadFootage(canvas.toBuffer())
-
-    const timeSpent = new Date().getTime() - start.getTime()
-    console.log(`Total time spent: ${timeSpent} ms`)
-    res.json({
-      peopleCount: people.length,
-      timeSpent: `${timeSpent} ms`,
-    })
+  const people = (await objectLocalize(canvas.toBuffer())).filter(
+    (object) => object.name === 'Person'
+  )
+  for (const person of people) {
+    drawBox(canvas, person.boundingPoly.normalizedVertices)
   }
-)
+
+  updatePeopleCount(people.length)
+  await uploadFootage(canvas.toBuffer())
+
+  const timeSpent = new Date().getTime() - start.getTime()
+  console.log(`Total time spent: ${timeSpent} ms`)
+  res.json({
+    peopleCount: people.length,
+    timeSpent: `${timeSpent} ms`,
+  })
+})
 
 app.listen(port, () => {
   console.log(`APP - App running on http://localhost:${port}`)
