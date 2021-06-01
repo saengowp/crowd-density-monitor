a45a8e4449bb6266b4ed08732a095616e389bd4d
PoraRee <ree_inter5@hotmail.com>
2021-06-01 18:22:36 +0700

    Modified counter can increase/decrease

--- frontend/index.js
+++ frontend/index.js
@@ -2,10 +2,25 @@ var cnt = document.getElementById("NumberCount");
 function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
+
 async function Counter() {
-  for (var i = 0; i <= 68; i++) {
-    await sleep(15);
-    cnt.innerHTML = i.toString();
+  var start = 0; //get from recent count
+  var end = 68; //get new count from storage
+  if (start < end)
+    for (var i = start; i <= end; i++) {
+      await sleep(15);
+      cnt.innerHTML = i.toString();
+    }
+  else {
+    for (var i = start; i >= end; i--) {
+      await sleep(15);
+      cnt.innerHTML = i.toString();
+    }
   }
 }
 Counter();
+
+var delayTime = 60000 * 1;
+function updateData() {
+  console.log("Update");
+}
